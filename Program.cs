d2cff98d70a66e8b5fd8aef1a5a73db4460b3bf2
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-18 21:54:29 +0530

    Ability to Create a Stack

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StackDemoProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine("..............");
+
+            foreach (int element in stack)
+            {
+                Console.WriteLine(element);
+            }
+
+
+            Console.WriteLine("...............");
+
+        }
+    }
+}
