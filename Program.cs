dc992ecd6b4453b384883012b9c9bcdd44d7d034
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-18 21:40:25 +0530

    Welcome to Linkedlist and Stack & Queues Problems

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Day14Problem
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to Linkedlist and Stack & Quesues Problem");
+        }
+    }
+}
